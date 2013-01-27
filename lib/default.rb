e3c5b38c036acc633210604ce8d4239ede0ad547
Nelson Chen <crazysim@gmail.com>
2013-01-26 20:27:08 -0800

    This appears to have fixed the stack level overflow problems

--- lib/default.rb
+++ lib/default.rb
@@ -1,2 +1,12 @@
 # All files in the 'lib' directory will be loaded
 # before nanoc starts compiling.
+
+# https://github.com/jingoro/nanoc-html5boilerplate/blob/master/lib/default.rb
+
+unless defined? LOADED_DEFAULT_CONFIG
+  LOADED_DEFAULT_CONFIG = true
+
+  require 'compass'
+  Compass.add_project_configuration 'compass/config.rb' # when using Compass 0.10
+  include Nanoc::Helpers::Rendering
+end
