6cf2160f980e4903fe7b8f5cabe0fe16c4f377e6
Nelson Chen <crazysim@gmail.com>
2013-02-03 00:58:14 -0800

    Relative assets true for compass

--- compass/config.rb
+++ compass/config.rb
@@ -3,6 +3,7 @@ project_path = "."
 css_dir      = "output/assets/stylesheets"
 sass_dir     = "content/assets/stylesheets"
 images_dir   = "output/assets/images"
+relative_assets = true
 
 # when using SCSS:
 sass_options = {
