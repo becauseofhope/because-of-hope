11057da98a03e007c97490eb8c24ad8bdfb1f35a
Nelson Chen <crazysim@gmail.com>
2013-02-03 00:57:33 -0800

    fix whitespace

--- compass/config.rb
+++ compass/config.rb
@@ -1,7 +1,7 @@
-http_path    = "/" 
-project_path = "." 
-css_dir      = "output/assets/stylesheets" 
-sass_dir     = "content/assets/stylesheets" 
+http_path    = "/"
+project_path = "."
+css_dir      = "output/assets/stylesheets"
+sass_dir     = "content/assets/stylesheets"
 images_dir   = "output/assets/images"
 
 # when using SCSS:
