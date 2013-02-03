9f0fa3d42193e58c0c7b606c85ad0b5b57c59bb3
Nelson Chen <crazysim@gmail.com>
2013-02-03 01:51:32 -0800

    compromise for foundation orbit

--- compass/config.rb
+++ compass/config.rb
@@ -2,7 +2,7 @@ http_path    = "/"
 project_path = "."
 css_dir      = "output/assets/stylesheets"
 sass_dir     = "content/assets/stylesheets"
-images_dir   = "output/assets/images"
+images_dir   = "content/assets/images"
 relative_assets = true
 
 # when using SCSS:
